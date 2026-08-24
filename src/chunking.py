1fb65ccca34e7bed604fce416d2181bf17624288
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-08-24 11:35:57 +0500

    In chunking.py: Added EXTENSION_TO_LANGUAGE which converts .py to python language which will be used as parameter for language aware chunking

--- src/chunking.py
+++ src/chunking.py
@@ -1,6 +1,22 @@
 from typing import List
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter,Language
 from langchain_core.documents import Document
+
+EXTENSION_TO_LANGUAGE = {
+    ".py": Language.PYTHON,
+    ".js": Language.JS,
+    ".jsx": Language.JS,
+    ".ts": Language.TS,
+    ".tsx": Language.TS,
+    ".java": Language.JAVA,
+    ".go": Language.GO,
+    ".rs": Language.RUST,
+    ".cpp": Language.CPP,
+    ".c": Language.C,
+    ".rb": Language.RUBY,
+    ".php": Language.PHP,
+    ".html": Language.HTML,
+}
 
 def _chunking(docs: List[Document], chunk_size: int = 1000, chunk_overlap: int = 200) -> List[Document]:
     text_splitter = RecursiveCharacterTextSplitter(
